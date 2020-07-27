cf0f1664304c821b5dccb41fa7ffb28a2155ee9f
Colin Wang <colingwuyu@gmail.com>
2020-07-27 19:40:56 -0400

    Update qtable.py

--- hb/bots/qtablebot/qtable.py
+++ hb/bots/qtablebot/qtable.py
@@ -10,28 +10,14 @@ class QTable:
                  action_spec: market_specs.DiscretizedBoundedArray):
         self._obs_spec = observation_spec
         self._action_spec = action_spec
-        #
-        # mesh = np.meshgrid(*(np.arange(mi, ma+st, st) for mi, ma, st in zip(observation_spec.minimum,
-        #                                                                     observation_spec.maximum,
-        #                                                                     observation_spec.discretize_step)))
-        # flattened_mesh = np.array([m.flatten() for m in mesh])
         self._int_multipliers = [int_multiplier(
             disc) for disc in observation_spec.discretize_step]
-        # for i in range(flattened_mesh.shape[0]):
-        #     flattened_mesh[i] = (flattened_mesh[i] *
-        #                          self._int_multipliers[i])
-        # self._flattened_mesh = flattened_mesh.astype(np.int64)
-        # self._action_space = np.arange(action_spec.minimum[0], action_spec.maximum[0]+action_spec.discretize_step[0],
-        #                                action_spec.discretize_step[0])
-        # Uniform initialization
         action_num = round((action_spec.maximum - action_spec.minimum)[0] /
                            action_spec.discretize_step[0]) + 1
         self._action_space = np.arange(action_spec.minimum[0], action_spec.maximum[0]+action_spec.discretize_step[0],
                                        action_spec.discretize_step[0])
         self._action_init = np.zeros(int(action_num))
-        # bias to buy action for initialization
         self.qtable = {}
-        # self._qtable = np.tile(self._action_space / 10, (self._flattened_mesh.shape[1], 1))
 
     def _encoding_obs(self, observation: np.ndarray):
         encoded_obs = observation.copy().astype(str)
@@ -56,12 +42,6 @@ class QTable:
     @property
     def action_space(self):
         return self._action_space
-
-    # def _coding_observation(self, observation: np.ndarray):
-    #     select_ind = np.array([True]*self._flattened_mesh.shape[1])
-    #     for obs_i, obs in enumerate(observation):
-    #         select_ind = select_ind & (self._flattened_mesh[obs_i] == round(obs*self._int_multipliers[obs_i]))
-    #     return np.where(select_ind)[0][0]
 
     def select_maxQ_action(self, observation: np.ndarray):
         obs_ind = self._encoding_obs(observation)
