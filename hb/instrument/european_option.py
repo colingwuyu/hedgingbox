6a0bb2672b8b523eb9a35be7da1f25f125a1e052
Colin <colingwuyu@Gmail.com>
2020-09-25 12:19:20 -0400

    update get_pred_price for european_option to generate price on the fly for prediction

--- hb/instrument/european_option.py
+++ hb/instrument/european_option.py
@@ -147,7 +147,7 @@ class EuropeanOption(Instrument):
         Returns:
             float: prediction price
         """
-        if self._cur_pred_file == "Pred":
+        if self._cur_pred_file == "Pred_price.csv":
             return self.get_sim_price()
         self.set_pricing_engine()
         self._pred_ind += 1
