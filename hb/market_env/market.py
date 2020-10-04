d3d5270949deb9e53da6a88e09bfdb50060c55cf
Colin <colingwuyu@Gmail.com>
2020-10-03 20:24:16 -0400

    remove print

--- hb/market_env/market.py
+++ hb/market_env/market.py
@@ -347,9 +347,7 @@ class Market(dm_env.Environment):
         #   Cashflow at Time t                                          #
         #   Transaction Cost at Time t.                                 #
         # ==============================================================#
-        print(action)
         cashflow, trans_cost = self._portfolio.rebalance(action)        #
-        print(action)
         trans_cost += self._event_trans_cost                            #
         # add any cashflow in/out to funding account                    #
         self._cash_account.add(cashflow)                                #
