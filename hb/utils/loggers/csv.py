35fe6d5c9eb4f37d9e1edd7a5e30d563e68a4d3c
Zeyu Wang <colingwuyu@gmail.com>
2021-09-02 22:48:43 +0000

    add close method to csvlogger to adopt new amce logger interface

--- hb/utils/loggers/csv.py
+++ hb/utils/loggers/csv.py
@@ -13,50 +13,57 @@ from acme.utils.loggers import base
 
 
 class CSVLogger(base.Logger):
-  """Standard CSV logger."""
+    """Standard CSV logger."""
 
-  _open = open
+    _open = open
 
-  def __init__(self,
-               directory: str = '~/acme',
-               label: str = '',
-               time_delta: float = 0.,
-               add_uid: bool = False):
-    directory = paths.process_path(os.path.join(directory, 'logs', label), add_uid=add_uid)
-    self._file_path = os.path.join(directory, 'logs.csv')
-    logging.info('Logging to %s', self._file_path)
-    self._time = time.time()
-    self._time_delta = time_delta
-    if os.path.exists(self._file_path):
-        self._header_exists = True
-    else:
-        self._header_exists = False
+    def __init__(
+        self,
+        directory: str = "~/acme",
+        label: str = "",
+        time_delta: float = 0.0,
+        add_uid: bool = False,
+    ):
+        directory = paths.process_path(
+            os.path.join(directory, "logs", label), add_uid=add_uid
+        )
+        self._file_path = os.path.join(directory, "logs.csv")
+        logging.info("Logging to %s", self._file_path)
+        self._time = time.time()
+        self._time_delta = time_delta
+        if os.path.exists(self._file_path):
+            self._header_exists = True
+        else:
+            self._header_exists = False
 
-  def write(self, data: base.LoggingData):
-    """Writes a `data` into a row of comma-separated values."""
+    def write(self, data: base.LoggingData):
+        """Writes a `data` into a row of comma-separated values."""
 
-    # Only log if `time_delta` seconds have passed since last logging event.
-    now = time.time()
-    if now - self._time < self._time_delta:
-      return
-    self._time = now
+        # Only log if `time_delta` seconds have passed since last logging event.
+        now = time.time()
+        if now - self._time < self._time_delta:
+            return
+        self._time = now
 
-    # Append row to CSV.
-    with self._open(self._file_path, mode='a') as f:
-      data = base.to_numpy(data)
-      keys = sorted(data.keys())
-      writer = csv.DictWriter(f, fieldnames=keys)
-      if not self._header_exists:
-        # Only write the column headers once.
-        writer.writeheader()
-        self._header_exists = True
-      writer.writerow(data)
+        # Append row to CSV.
+        with self._open(self._file_path, mode="a") as f:
+            data = base.to_numpy(data)
+            keys = sorted(data.keys())
+            writer = csv.DictWriter(f, fieldnames=keys)
+            if not self._header_exists:
+                # Only write the column headers once.
+                writer.writeheader()
+                self._header_exists = True
+            writer.writerow(data)
 
-  @property
-  def file_path(self) -> str:
-    return self._file_path
+    @property
+    def file_path(self) -> str:
+        return self._file_path
 
-  def clear(self):
-    if self._header_exists:
-        os.remove(self._file_path)
-        self._header_exists = False
+    def clear(self):
+        if self._header_exists:
+            os.remove(self._file_path)
+            self._header_exists = False
+
+    def close(self) -> None:
+        return
